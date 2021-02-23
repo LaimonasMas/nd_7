5339583ab1ffe26f7192dd58135a5f398528f1e9
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-02-24 00:45:33 +0200

    4 done

---
+++ lemon.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+header('Location: http://localhost/nd/nd_7/orange.php/');
+exit;
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: lemonchiffon;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Čia 'lemon.php' failas!!!</h1>
+
+</body>
+
+</html>
