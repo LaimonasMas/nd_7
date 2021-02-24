4a5a84fe03e9c6415c9b74f07983dfa2e8a962a3
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-02-24 09:16:19 +0200

    fixed bug in nr.5

--- blue.php
+++ blue.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,18 +10,19 @@
         body {
             background-color: blue;
         }
+
+        <?php
+        if ($_GET['redirect'] == 'red') {
+            header('Location: http://localhost/nd/nd_7/red.php/');
+            exit;
+        }
+        ?>
     </style>
 </head>
 
 <body>
     <a href="?redirect=red" style="color: white">http://localhost/nd/nd_7/blue.php/</a>
-    <?php
 
-if ($_GET['redirect'] == 'red') {
-    header('Location: http://localhost/nd/nd_7/red.php/');
-    exit;
-} 
-?>
 </body>
 
 </html>
--- red.php
+++ red.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,18 +10,19 @@
         body {
             background-color: red;
         }
+
+        <?php
+        if ($_GET['redirect'] == 'blue') {
+            header('Location: http://localhost/nd/nd_7/blue.php/');
+            exit;
+        }
+        ?>
     </style>
 </head>
 
 <body>
     <a href="?redirect=blue">http://localhost/nd/nd_7/red.php/</a>
-    <?php
 
-if ($_GET['redirect'] == 'blue') {
-    header('Location: http://localhost/nd/nd_7/blue.php/');
-    exit;
-}
-?>
 </body>
 
 </html>
